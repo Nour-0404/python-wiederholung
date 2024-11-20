2b3b3d47c81462f03d2eef196a290c7c434bc655
Nour Alkabiry <malkabiry@schueler.hakzell.at>
2024-11-20 09:21:52 +0100

    verzweigun

---
+++ verzweigung.py
@@ -0,0 +1,14 @@
+# verzweigung.py
+
+zahl = 10 #Zahl vom Benutzer
+
+if zahl > 5:
+    print("Die Zahl is größer als 5")
+elif zahl == 5:
+    print("die Zahl ist 5.")
+else:
+    print("Die Zahl ist kleiner als 5")
+
+
+gi
+
