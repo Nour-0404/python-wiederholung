fa9926ebfd4d1fcfd287ef606305c6bc47da70d6
Nour Alkabiry <malkabiry@schueler.hakzell.at>
2024-11-21 11:03:10 +0100

    add schleifen

---
+++ schleifen.py
@@ -0,0 +1,12 @@
+# schleifen.py
+
+# Ausgabe der Zahlen von 0 bis 4s
+for zahl in range (5):
+    print(zahl)
+
+
+fruits = ["Apfel", "Banane", "Kirsche"]
+fruits.append("Birne")
+
+for fruits in fruits:
+    print(fruits)
